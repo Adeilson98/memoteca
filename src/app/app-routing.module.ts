b3676b14f3365dae022ac9b24e5aaab68f0cf090
Adeilson Silva <adeilson.silva@agenciagenia.com.br>
2022-11-28 17:10:49 -0300

    router ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
+import { ListThoughtsComponent } from './components/thoughts/list-thoughts/list-thoughts.component';
+import { CreateThoughtsComponent } from './components/thoughts/create-thoughts/create-thoughts.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'mural',
+    pathMatch: 'full' // whem full is set all url is read
+  },
+  {
+    path: 'novo-pensamento',
+    component: CreateThoughtsComponent
+  },
+  {
+    path: 'mural',
+    component: ListThoughtsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
